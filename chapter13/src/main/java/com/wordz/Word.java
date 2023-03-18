df3339b5b96b8f1dbd5d71b8b88e2c8d84af08a4
KonstantinGlady <gladyi@hotmail.com>
2023-03-18 10:19:19 +0100

    oneCorrectLetter Green

--- chapter13/src/main/java/com/wordz/Word.java
+++ chapter13/src/main/java/com/wordz/Word.java
@@ -3,12 +3,17 @@ package com.wordz;
 import java.io.StreamCorruptedException;
 
 public class Word {
+    private final String word;
+
     public Word(String correctWord) {
+        word = correctWord;
     }
 
     public Score guess(String attempt) {
 
-        var score = new Score();
+        var score = new Score(word);
+
+        score.access(0, attempt);
 
         return score;
     }
