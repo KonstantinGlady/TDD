bbce412095fa4f6337b998a44715e5622bfb10fc
KonstantinGlady <gladyi@hotmail.com>
2023-03-18 10:08:19 +0100

    oneIncorrectLetter Red

---
+++ chapter13/src/main/java/com/wordz/Word.java
@@ -0,0 +1,15 @@
+package com.wordz;
+
+import java.io.StreamCorruptedException;
+
+public class Word {
+    public Word(String correctWord) {
+    }
+
+    public Score guess(String attempt) {
+
+        var score = new Score();
+
+        return score;
+    }
+}
---
+++ chapter13/src/test/java/com/wordz/WordTest.java
@@ -0,0 +1,18 @@
+package com.wordz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class WordTest {
+
+    @Test
+    void oneIncorrectLetter() {
+
+        var word = new Word("A");
+        var score = word.guess("Z");
+        var result = score.letter(0);
+
+        assertThat(result).isEqualTo(Letter.INCORRECT);
+    }
+}
