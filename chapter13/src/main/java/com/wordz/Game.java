c9e02b914fd8f972c68f1852bf6894e84bcaa72e
KonstantinGlady <gladyi@hotmail.com>
2023-03-21 18:41:55 +0100

    GameRepository mock Green

--- chapter13/src/main/java/com/wordz/Game.java
+++ chapter13/src/main/java/com/wordz/Game.java
@@ -1,15 +1,26 @@
 package com.wordz;
 
 public class Game {
+    private final Player player;
+    private final String word;
+    private final int attempt;
+
+    public Game(Player player, String word, int attempt) {
+
+        this.player = player;
+        this.word = word;
+        this.attempt = attempt;
+    }
+
     public String getWord() {
-        return null;
+        return word;
     }
 
     public int getAttemptNumbers() {
-        return 0;
+        return attempt;
     }
 
     public Player getPlayer() {
-        return null;
+        return player;
     }
 }
--- chapter13/src/main/java/com/wordz/Wordz.java
+++ chapter13/src/main/java/com/wordz/Wordz.java
@@ -1,7 +1,18 @@
 package com.wordz;
 
 public class Wordz {
+
+
+    private final GameRepository gameRepository;
+
+    public Wordz(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
     public void newGame(Player player) {
 
+        var game = new Game(player, "ARISE", 0);
+
+        gameRepository.create(game);
     }
 }
