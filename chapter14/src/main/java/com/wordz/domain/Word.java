600fc7bae36b7e9164fba2427ce2697589640143
KonstantinGlady <gladyi@hotmail.com>
2023-04-01 18:15:13 +0200

    oneIncorrectLetter Red

---
+++ chapter14/src/main/java/com/wordz/domain/Word.java
@@ -0,0 +1,11 @@
+package com.wordz.domain;
+
+public class Word {
+    public Word(String correctWord) {
+    }
+
+    public Score guess(String attempt) {
+        Score score = new Score();
+        return null;
+    }
+}
