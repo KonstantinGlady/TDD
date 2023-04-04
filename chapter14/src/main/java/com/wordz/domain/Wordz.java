1fa60647946e67ef762af9a9a5e7eb2a66bd7b64
KonstantinGlady <gladyi@hotmail.com>
2023-04-04 19:57:45 +0200

    selectsRandomWord Red

--- chapter14/src/main/java/com/wordz/domain/Wordz.java
+++ chapter14/src/main/java/com/wordz/domain/Wordz.java
@@ -2,10 +2,12 @@ package com.wordz.domain;
 
 public class Wordz {
     private final GameRepository gameRepository;
+    private final WordSelection wordSelection;
 
-    public Wordz(GameRepository gameRepository) {
+    public Wordz(GameRepository gameRepository, WordRepository wordRepository, RandomNumber rnd) {
 
         this.gameRepository = gameRepository;
+        this.wordSelection = new WordSelection(wordRepository, rnd);
     }
 
     public void newGame(Player player) {
--- chapter14/src/test/java/com/wordz/domain/NewGameTest.java
+++ chapter14/src/test/java/com/wordz/domain/NewGameTest.java
@@ -8,14 +8,43 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class NewGameTest {
+
+    @Mock
+    private WordRepository wordRepository;
+    @Mock
+    private RandomNumber random;
     @Mock
     private GameRepository gameRepository;
     @InjectMocks
     private Wordz wordz;
+
+    @Test
+    void selectsRandomWord() {
+
+        givenWordToSelect("ABCDE");
+
+        Player player = new Player();
+        wordz.newGame(player);
+
+        var game = getGameInRepository();
+
+        assertThat(game.getWord()).isEqualTo("ABCDE");
+    }
+
+    private void givenWordToSelect(String wordToSelect) {
+        int wordNumber = 2;
+
+        when(random.next(anyInt()))
+                .thenReturn(wordNumber);
+        when(wordRepository.fetchWordByNumber(wordNumber))
+                .thenReturn(wordToSelect);
+    }
 
     @Test
     void startNewGame() {
@@ -31,7 +60,6 @@ class NewGameTest {
     }
 
     private Game getGameInRepository() {
-
         var gameArgument = ArgumentCaptor.forClass(Game.class);
         verify(gameRepository).create(gameArgument.capture());
 
