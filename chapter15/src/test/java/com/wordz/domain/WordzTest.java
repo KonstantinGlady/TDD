3e96c98b21a5212663ca9f67eda4b3364a9ff4b0
KonstantinGlady <gladyi@hotmail.com>
2023-04-22 14:59:23 +0200

    secondLetterWrongPosition red

--- chapter15/src/test/java/com/wordz/domain/WordzTest.java
+++ chapter15/src/test/java/com/wordz/domain/WordzTest.java
@@ -21,6 +21,13 @@ class WordzTest {
         assertScoreForLetter(score, 0, Letter.CORRECT);
     }
 
+    @Test
+    void secondLetterWrongPosition() {
+        var word = new Word("AR");
+        var score = word.guess("ZA");
+        assertScoreForLetter(score, 1, Letter.PART_CORRECT);
+    }
+
     private static void assertScoreForLetter(Score score, int position, Letter expected) {
         assertThat(score.letter(position)).isEqualTo(expected);
     }
